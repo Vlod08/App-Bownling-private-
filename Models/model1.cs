a9716fc870bea972a1c8dc5a2f7a4cab715a3f58
Maëva B <maeva.burillo@u-psud.fr>
2023-12-19 12:11:10 +0100

    squelette app

---
+++ Models/model1.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication2.Models
+{
+    public class model1
+    {
+    }
+
+    public class frame{
+        int t1, t2;
+    }
+    public class joueur
+    {
+        string name;
+        frame[] score = new frame[10];
+    }
+
+    public class partie
+    {
+        List<joueur> joeurs;
+
+        string gagnant;
+    }
+
+}
